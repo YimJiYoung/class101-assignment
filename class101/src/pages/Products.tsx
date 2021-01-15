2d423cf81abcf413222492d304081b389ffd038a
YimJiYoung <chiy323@gmail.com>
2021-01-15 19:32:45 +0900

    feat: 상품 목록 score 내림차순으로 정렬
    - Products 컴포넌트에서 products 내림차순으로 정렬하여 ProductList에 전달
    - 정렬하는 부분은 products가 변경되지 않았다면 리렌더링 시 재계산하지 않도록 useMemo 사용

--- class101/src/pages/Products.tsx
+++ class101/src/pages/Products.tsx
@@ -1,12 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useMemo } from 'react';
 import ProductList from '../components/common/ProductList';
 import productContext from '../contexts/productContext';
+import IProduct from '../types/product';
 
 function Products() {
   const products = useContext(productContext);
 
+  const sortProductByScore = (_products: IProduct[]) => [..._products].sort((a, b) => b.score - a.score);
+  const sortedProducts = useMemo(() => sortProductByScore(products), [products]);
+
   return (
-    <ProductList products={products.slice(0, 5)} />
+    <ProductList products={sortedProducts.slice(0, 5)} />
   );
 }
 
