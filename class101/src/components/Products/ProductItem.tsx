dc121e7d568e525f7f47ffb4c698cbb258ac1fbb
YimJiYoung <chiy323@gmail.com>
2021-02-01 16:26:32 +0900

    refactor: 성능 향상을 위한 ProductItem, ProductList 리팩토링
    - ProductItem에서 다른 아이템이 장바구니에 담길 때에도(자신은 영향이 없는 경우)
    globalContext를 사용하여 리렌더링이 발생 -> globalContext는 ProductList에서 사용하도록 수정
    - ProductItem에 대해 React.memo()로 감싸주어 props 같을 때 리렌더링 방지
    - ProductList에서 Item 컴포넌트로 넘겨주는 콜백 함수 useCallback으로 메모이제이션(재선언 방지)

--- class101/src/components/Products/ProductItem.tsx
+++ class101/src/components/Products/ProductItem.tsx
@@ -2,35 +2,22 @@ import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Card, Image } from 'antd';
 import CartButton from './CartButton';
-import { useGlobalContext } from '../../contexts/GlobalProvider';
-import { ADD_TO_CART, REMOVE_FROM_CART } from '../../constants/actions';
 import { Product } from '../../types';
 
 const { Meta } = Card;
 
 interface IProps {
     item: Product;
+    isInCart: boolean;
+    onCartButtonClick: (item: Product) => void;
 }
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
-const ProductItem = ({ item }: IProps) => {
+const ProductItem = ({ item, isInCart, onCartButtonClick }: IProps) => {
   const { title, coverImage, price } = item;
-  const [state, dispatch] = useGlobalContext();
-
-  const checkInCart = (id: string) => {
-    const index = state.cart.findIndex((_item) => _item.id === id);
-    return index !== -1;
-  };
-
-  const isInCart = useMemo(() => checkInCart(item.id), [item, state.cart]);
-
-  const handleCartButtonClick = () => {
-    if (!isInCart) dispatch({ type: ADD_TO_CART, payload: item });
-    else dispatch({ type: REMOVE_FROM_CART, payload: item.id });
-  };
 
   return (
     <Card
@@ -43,11 +30,11 @@ const ProductItem = ({ item }: IProps) => {
       <ButtonWrapper>
         <CartButton
           isInCart={isInCart}
-          handleClick={handleCartButtonClick}
+          handleClick={() => { onCartButtonClick(item); }}
         />
       </ButtonWrapper>
     </Card>
   );
 };
 
-export default ProductItem;
+export default React.memo(ProductItem);
--- class101/src/components/Products/ProductList.tsx
+++ class101/src/components/Products/ProductList.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Row, Col } from 'antd';
 import ProductItem from './ProductItem';
 import { Product } from '../../types';
+import { useGlobalContext } from '../../contexts/GlobalProvider';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../../constants/actions';
 
 interface IProps {
     products: Product[];
 }
 
-const ProductList = ({ products }: IProps) => (
-  <Row justify="space-around">
-    { products && products.map((product) => (
-      <Col xs={24} md={4} key={product.id}>
-        <ProductItem
-          item={product}
-        />
-      </Col>
-    ))}
-  </Row>
-);
+const ProductList = ({ products }: IProps) => {
+  const [state, dispatch] = useGlobalContext();
 
-export default ProductList;
+  const checkInCart = (id: string) => {
+    const index = state.cart.findIndex((_item) => _item.id === id);
+    return index !== -1;
+  };
+
+  const handleCartButtonClick = useCallback((item: Product) => {
+    if (!checkInCart(item.id)) dispatch({ type: ADD_TO_CART, payload: item });
+    else dispatch({ type: REMOVE_FROM_CART, payload: item.id });
+  }, []);
+
+  return (
+    <Row justify="space-around">
+      { products && products.map((product) => (
+        <Col xs={24} md={4} key={product.id}>
+          <ProductItem
+            item={product}
+            isInCart={checkInCart(product.id)}
+            onCartButtonClick={handleCartButtonClick}
+          />
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default React.memo(ProductList);
